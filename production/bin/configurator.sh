9cc58a316e43e8a4e28ec24657a1ea761d4b210b
Martin Peck <martin@meedan.com>
2021-08-09 17:11:35 -0700

    Be verbose about each step of configuration.

--- production/bin/configurator.sh
+++ production/bin/configurator.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Starting configuration..."
+
 if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
 	exit 1
@@ -10,9 +12,11 @@ fi
 cp config/cookies.json.example config/cookies.json
 
 if [[ "$DEPLOY_ENV" != "qa" && "$DEPLOY_ENV" != "live" ]]; then
-	# Only use the test configuration if we're not deploying to QA or Live.
+	# Only use the test configuration if we're not deploying to QA or Live, or running CI.
+	echo "Copying test .env_file into place."
 	cp .env_file.test .env_file
 else
 	# The dumb-init process expects an .env_file. Use an empty one for QA and Live.
+	echo "Using SSM env vars instead of .env_file."
 	touch .env_file
 fi
