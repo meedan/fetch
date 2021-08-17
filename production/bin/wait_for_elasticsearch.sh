0da82369e4962db21591551477a3b526effca52b
Martin Peck <martin@meedan.com>
2021-08-16 18:24:46 -0700

    Omit metrics when curl checks ElasticSearch status.

--- production/bin/wait_for_elasticsearch.sh
+++ production/bin/wait_for_elasticsearch.sh
@@ -5,8 +5,8 @@ source .env_file
 set +a
 
 echo "Waiting for ElasticSearch to become available..."
-until curl --silent -XGET --fail ${es_host}; do printf '.'; sleep 1; done
+until curl --silent -XGET --fail ${es_host} 2>/dev/null; do printf '.'; sleep 1; done
 echo "Finished waiting."
 echo ""
 echo "Available indexes:"
-curl ${es_host}/_aliases?pretty=true
+curl ${es_host}/_aliases?pretty=true 2>/dev/null
