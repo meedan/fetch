e3160bdf65069819db5c3f7be7c4d6764046807a
Martin Peck <martin@meedan.com>
2021-08-30 09:39:46 -0700

    Update rspec test syntax to avoid warnings when running integration tests.

--- spec/lib/db_integration_test.rb
+++ spec/lib/db_integration_test.rb
@@ -3,13 +3,13 @@ describe 'integration test with ElasticSearch' do#, integration: true do
   include Rack::Test::Methods
 
   def app
-    Site
+    Site.new
   end
 
   before do
     @storage_results ||= {}
     WebMock.allow_net_connect!
-    Mafindo.any_instance.stub(:get_authors).and_return([{"id" => 36, "nama"=>"blah", "website"=>"blah"}])
+    allow_any_instance_of(Mafindo).to receive(:get_authors).and_return([{"id" => 36, "nama"=>"blah", "website"=>"blah"}])
     ClaimReviewParser.enabled_subclasses.reject{|x| x.service.to_s.include?("#")}.each do |subclass|
       raw = JSON.parse(File.read("spec/fixtures/#{subclass.service}_raw.json"))
       raw['page'] = Nokogiri.parse(raw['page']) if raw['page']
