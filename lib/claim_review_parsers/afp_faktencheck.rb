75c62407ce83c216ce52bbc6563babac71d6676b
Devin Gaffney <itsme@devingaffney.com>
2021-09-02 09:20:39 -0700

    CHECK-886 fix small parsing discrepancy

--- lib/claim_review_parsers/afp_faktencheck.rb
+++ lib/claim_review_parsers/afp_faktencheck.rb
@@ -7,4 +7,10 @@ class AFPFaktencheck < AFP
   def hostname
     'https://faktencheck.afp.com'
   end
+
+  def parse_raw_claim_review(raw_claim_review)
+    parsed = super(raw_claim_review)
+    parsed[:claim_review_headline] = (claim_review["@graph"][0]["claimReviewed"] || claim_review_headline_from_raw_claim_review_and_claim_review(raw_claim_review, claim_review))
+    parsed
+  end
 end
