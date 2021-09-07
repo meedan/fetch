5add654bd172cbc3f49f2bd0c873a4299f72d786
Devin Gaffney <itsme@devingaffney.com>
2021-09-07 07:51:10 -0700

    CHECK-900 fix error

--- lib/alegre_client.rb
+++ lib/alegre_client.rb
@@ -6,7 +6,7 @@ class AlegreClient
   def self.get_enrichment_for_url(url)
     JSON.parse(
       RestClient.post(
-        self.host+"/article/",
+        self.host+"article/",
         {url: url}.to_json,
         {content_type: 'application/json'}
       )
