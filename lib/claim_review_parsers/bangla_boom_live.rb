2dab6b7683db32524f090c4ab42db037318eca2d
Devin Gaffney <dgaffney@devins-mbp.lan>
2021-10-26 09:24:09 -0700

    CHECK-926 add two boom live publications

---
+++ lib/claim_review_parsers/bangla_boom_live.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Parser for http://hindi.boomlive.in/ - does not follow standard Pagination scheme from PaginatedReviewClaims!
+class BanglaBoomLive < ClaimReviewParser
+  include PaginatedReviewClaims
+  def hostname
+    'http://bangla.boomlive.in'
+  end
+
+  def fact_list_path(page = 1)
+    # appears to be zero-indexed
+    "/fast-check/#{page}"
+  end
+
+  def url_extraction_search
+    'main#main div.category-articles-list h2.entry-title a'
+  end
+
+  def url_extractor(atag)
+    hostname + atag.attributes['href'].value
+  end
+
+  def parse_raw_claim_review(raw_claim_review)
+    article = extract_ld_json_script_block(raw_claim_review["page"], 0)
+    {
+      id: raw_claim_review["url"],
+      created_at: Time.parse(article['datePublished']),
+      author: article["author"]["name"],
+      author_link: hostname+article["author"]["url"],
+      claim_review_headline: article["claimReviewed"],
+      claim_review_body: raw_claim_review["page"].search("div.short-factcheck-snippet").text,
+      claim_review_reviewed: article["itemReviewed"]["name"],
+      claim_review_image_url: article["image"]["contentUrl"],
+      claim_review_result: article["reviewRating"]["alternateName"],
+      claim_review_result_score: claim_result_score_from_raw_claim_review(article["reviewRating"]),
+      claim_review_url: raw_claim_review['url'],
+      raw_claim_review: raw_claim_review
+    }
+  end
+end
---
+++ spec/lib/claim_review_parsers/hindi_boom_live_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+describe HindiBoomLive do
+  describe 'instance' do
+    it 'has a hostname' do
+      expect(described_class.new.hostname).to(eq('http://hindi.boomlive.in'))
+    end
+
+    it 'has a fact_list_path' do
+      expect(described_class.new.fact_list_path(1)).to(eq('/fast-check/1'))
+    end
+
+    it 'has a url_extraction_search' do
+      expect(described_class.new.url_extraction_search).to(eq('main#main div.category-articles-list h2.entry-title a'))
+    end
+
+    it 'extracts a url' do
+      expect(described_class.new.url_extractor(Nokogiri.parse("<a href='/blah'>wow</a>").search('a')[0])).to(eq('http://hindi.boomlive.in/blah'))
+    end
+
+    it 'parses a raw_claim_review' do
+      raw = JSON.parse(File.read('spec/fixtures/hindi_boom_live_raw.json'))
+      raw['page'] = Nokogiri.parse(raw['page'])
+      parsed_claim = described_class.new.parse_raw_claim_review(raw)
+      expect(parsed_claim.class).to(eq(Hash))
+      ClaimReview.mandatory_fields.each do |field|
+        expect(QuietHashie[parsed_claim][field].nil?).to(eq(false))
+      end
+    end
+  end
+end
