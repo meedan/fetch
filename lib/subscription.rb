41049a06fa93eac462a7df59c8a73ccab5cb5e2d
Devin Gaffney <dgaffney@devins-mbp.lan>
2021-12-09 09:39:02 -0800

    CHECK-1203 Store subscriptions in elasticsearch

--- lib/subscription.rb
+++ lib/subscription.rb
@@ -7,35 +7,20 @@ class Subscription
     Digest::MD5.hexdigest(url)+"_params"
   end
 
-  def self.get_existing_params_for_url(url)
-    raw_params = $REDIS_CLIENT.get(self.url_params_key(url))
-    if !raw_params.nil?
-      params = JSON.parse(raw_params)
-    else
-      params = {}
-    end
-    params
-  end
-
-  def self.store_params_for_url(url, existing_params)
-    $REDIS_CLIENT.set(self.url_params_key(url), existing_params.to_json)
-  end
-
-  def self.delete_params_for_url(url)
-    $REDIS_CLIENT.del(self.url_params_key(url))
+  def self.get_existing_params_for_url(service, url)
+    StoredSubscription.get_subscription_for_url(service, url)['params']
   end
 
   def self.add_subscription(services, urls, languages=nil)
     languages = [languages].flatten.compact
     [services].flatten.collect do |service|
       [urls].flatten.collect do |url|
-        $REDIS_CLIENT.sadd(self.keyname(service), url)
-        existing_params = self.get_existing_params_for_url(url)
+        existing_params = self.get_existing_params_for_url(service, url)
         existing_params["language"] ||= []
         languages.each do |language|
           existing_params["language"] << language
         end
-        self.store_params_for_url(url, existing_params)
+        StoredSubscription.store_subscription(service, url, existing_params)
       end
     end.flatten
   end
@@ -43,16 +28,15 @@ class Subscription
   def self.remove_subscription(services, urls)
     [services].flatten.collect do |service|
       [urls].flatten.collect do |url|
-        $REDIS_CLIENT.srem(self.keyname(service), url)
-        self.delete_params_for_url(url)
+        StoredSubscription.delete_subscription(service, url)
       end
     end.flatten
   end
 
   def self.get_subscriptions(services)
     Hash[[services].flatten.collect do |service|
-      webhooks = $REDIS_CLIENT.smembers(self.keyname(service)) || []
-      [service, Hash[webhooks.collect{|url| [url, self.get_existing_params_for_url(url)]}]]
+      webhooks = StoredSubscription.get_subscriptions_for_service(service)
+      [service, Hash[webhooks.collect{|url| [url, self.get_existing_params_for_url(service, url)]}]]
     end]
   end
 
