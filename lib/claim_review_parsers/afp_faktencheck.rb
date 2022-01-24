75fbe9be7a4908abb65a342b4b070ad16059f313
Devin Gaffney <dgaffney@devins-mbp.lan>
2022-01-24 08:20:22 -0800

    CHECK-1268 alter body of claim review to only be concluding remarks

--- lib/claim_review_parsers/afp_faktencheck.rb
+++ lib/claim_review_parsers/afp_faktencheck.rb
@@ -8,9 +8,19 @@ class AFPFaktencheck < AFP
     'https://faktencheck.afp.com'
   end
 
+  def get_text_blocks(raw_claim_review)
+    raw_claim_review["page"].search("article div.article-entry p").collect(&:text)
+  end
+
+  def get_index_for_conclusion_paragraph(raw_claim_review)
+    raw_claim_review["page"].search("article div.article-entry p").collect(&:to_s).each_with_index.select{|h, i| h.include?("<strong>Fazit")}.last.last rescue 0
+  end
+
   def parse_raw_claim_review(raw_claim_review)
     parsed = super(raw_claim_review)
-    parsed[:claim_review_headline] = (parsed[:claim_review_headline] || parsed[:raw_claim_review]["@graph"][0]["claimReviewed"])
+    conclusion_index = get_index_for_conclusion_paragraph(raw_claim_review)
+    parsed[:claim_review_body] = get_text_blocks(raw_claim_review)[conclusion_index..-1]
+    parsed
     parsed
   end
 end
