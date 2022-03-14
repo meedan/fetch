0bda898bad70aee68160f232009a694356f38b2a
Devin Gaffney <itsme@devingaffney.com>
2022-03-14 10:37:54 -0700

    CHECK-1519 get rid of task params

--- spec/tasks/check_for_stale_parsers_test.rb
+++ spec/tasks/check_for_stale_parsers_test.rb
@@ -4,7 +4,7 @@ describe CheckForStaleParsers do
   describe 'instance' do
     it 'responds to perform' do
       API.stub(:services).and_return({services: [{:service=>"tempo_cekfakta", :count=>6889, :earliest=>"2018-01-16", :latest=>(Time.now-60*60*24*10).strftime("%Y-%m-%d")}]})
-      response = described_class.new.perform('blah', {})
+      response = described_class.new.perform
       expect(response).to(eq(["tempo_cekfakta"]))
     end
   end
