956988e6c320660fecc9f88f281d21762c64c8b4
Devin Gaffney <itsme@devingaffney.com>
2022-03-15 11:59:49 -0700

    CHECK-1594 add AlterMidya

--- lib/claim_review_parsers/alter_midya.rb
+++ lib/claim_review_parsers/alter_midya.rb
@@ -1,21 +1,21 @@
 
 # frozen_string_literal: true
 
-# Parser for https://mindanaogoldstardaily.com/category/factsfirstph-2/
-class MindanaoGoldStarDaily < ClaimReviewParser
+# Parser for https://www.altermidya.net/category/factsfirstph/page/2/
+class AlterMidya < ClaimReviewParser
   include PaginatedReviewClaims
 
   def hostname
-    'https://mindanaogoldstardaily.com'
+    'https://www.altermidya.net'
   end
 
   def fact_list_path(page = 1)
-    "/category/factsfirstph-2/page/#{page}/"
+    "/category/factsfirstph/page/#{page}/"
   end
 
 
   def url_extraction_search
-    'div.tdc-content-wrap div.td-module-meta-info p.entry-title a'
+    'div.bkpage-content h4.title a'
   end
 
   def url_extractor(atag)
@@ -27,7 +27,7 @@ class MindanaoGoldStarDaily < ClaimReviewParser
   end
 
   def claim_review_headline_from_article_or_raw_claim_review(article, raw_claim_review)
-    article && article["headline"] || raw_claim_review["page"].search("div.s-post-header h1").text
+    article && article["headline"] || raw_claim_review["page"].search("h1.entry-title").text
   end
 
   def claim_review_image_url_from_article_or_raw_claim_review(article, raw_claim_review)
@@ -39,10 +39,10 @@ class MindanaoGoldStarDaily < ClaimReviewParser
     {
       id: raw_claim_review['url'],
       created_at: created_at_from_article_or_raw_claim_review(article, raw_claim_review),
-      author: raw_claim_review["page"].search("div.post-author a").text,
-      author_link: (raw_claim_review["page"].search("div.post-author a")[0].attributes["href"].value rescue nil),
+      author: raw_claim_review["page"].search("div.td-post-author-name a").text,
+      author_link: (raw_claim_review["page"].search("div.td-post-author-name a")[0].attributes["href"].value rescue nil),
       claim_review_headline: claim_review_headline_from_article_or_raw_claim_review(article, raw_claim_review),
-      claim_review_body: raw_claim_review["page"].search("div.article-wrap div.article-content p").collect(&:text).join(" "),
+      claim_review_body: raw_claim_review["page"].search("div.td-ss-main-content div.td-post-content p").collect(&:text).join(" "),
       claim_review_image_url: claim_review_image_url_from_article_or_raw_claim_review(article, raw_claim_review),
       claim_review_url: raw_claim_review['url']
     }
