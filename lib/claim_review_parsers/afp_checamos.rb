df3de11c140021cb408e82996f1406c54da4caae
Devin Gaffney <itsme@devingaffney.com>
2022-03-18 06:38:13 -0700

    CHECK-1600 update checamos article body (#197)

--- lib/claim_review_parsers/afp_checamos.rb
+++ lib/claim_review_parsers/afp_checamos.rb
@@ -15,7 +15,7 @@ class AFPChecamos < AFP
   def parse_raw_claim_review(raw_claim_review)
     parsed = super(raw_claim_review)
     new_image_url = get_image_url_for_raw_claim_review(raw_claim_review)
-    parsed[:claim_review_body] = claim_review_body_from_raw_claim_review(raw_claim_review)
+    parsed[:claim_review_body] = raw_claim_review["page"].search("article div.article-entry h3").text
     parsed[:claim_review_image_url] = new_image_url if !new_image_url.nil?
     parsed
   end
