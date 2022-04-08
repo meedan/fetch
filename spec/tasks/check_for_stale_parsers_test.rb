2f3ff1ea2dde302104e41ea0b52a6c7b401a9c34
Devin Gaffney <itsme@devingaffney.com>
2022-04-08 08:33:36 -0700

    CHECK-1729 fix test

--- spec/tasks/check_for_stale_parsers_test.rb
+++ spec/tasks/check_for_stale_parsers_test.rb
@@ -3,9 +3,9 @@
 describe CheckForStaleParsers do
   describe 'instance' do
     it 'responds to perform' do
-      API.stub(:services).and_return({services: [{:service=>"out_of_date_parser", :count=>6889, :earliest=>"2018-01-16", :latest=>(Time.now-60*60*24*10).strftime("%Y-%m-%d")}, {:service=>"other_parser", :count=>6889, :earliest=>"2018-01-16", :latest=>(Time.now-60*60*24*1).strftime("%Y-%m-%d")}]})
+      API.stub(:services).and_return({services: [{:service=>"mafindo", :count=>6889, :earliest=>"2018-01-16", :latest=>(Time.now-60*60*24*10).strftime("%Y-%m-%d")}, {:service=>"other_parser", :count=>6889, :earliest=>"2018-01-16", :latest=>(Time.now-60*60*24*1).strftime("%Y-%m-%d")}]})
       response = described_class.new.perform
-      expect(response).to(eq(["out_of_date_parser"]))
+      expect(response).to(eq(["mafindo"]))
     end
   end
 end
