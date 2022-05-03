eaa8f63e7e80fc76ab20ba949a4cf01eaa9d93b1
Devin Gaffney <itsme@devingaffney.com>
2022-05-03 12:12:40 -0700

    CHECK-1789 i legitimately have no idea, replacing new parser with contents from another parser entirely

--- lib/claim_review_parsers/la_silla_vacia.rb
+++ lib/claim_review_parsers/la_silla_vacia.rb
@@ -4,50 +4,85 @@
 class LaSillaVacia < ClaimReviewParser
   include PaginatedReviewClaims
   def hostname
-    'https://www.lasillavacia.com'
+    'https://www.altnews.in/'
   end
 
   def fact_list_path(page = 1)
-    # appears to be zero-indexed
-    "/elementAjax/SillaDetector/MasHistoriasRecientes?page=#{page}"
+    "/page/#{page}/"
   end
 
   def url_extraction_search
-    'a'
+    'article .entry-title a'
   end
 
   def url_extractor(atag)
     atag.attributes['href'].value
   end
 
-  def get_title_classes_from_raw_claim_review(raw_claim_review)
-    raw_claim_review["page"].search("div.detector-article h1.h2").first.attributes["class"].value
-  end
-
-  def claim_review_result_and_score_from_title_classes(title_classes)
-    if title_classes.include?("border-scale-red")
-      return [0.0, "False"]
-    elsif title_classes.include?("border-scale-orange")
-      return [0.33, "Mostly False"]
-    elsif title_classes.include?("border-scale-ligth-green")
-      return [0.66, "Mostly True"]
-    elsif title_classes.include?("border-scale-green")
-      return [1.0, "True"]
-    end
-  end
-
   def parse_raw_claim_review(raw_claim_review)
-    timestamp = Time.parse(raw_claim_review["page"].search("div.detector-article time.p").first.text.strip) rescue nil
-    claim_review_result, claim_review_result_score = claim_review_result_and_score_from_title_classes(get_title_classes_from_raw_claim_review(raw_claim_review))
+    claim_review = extract_ld_json_script_block(raw_claim_review["page"], 0, "script.yoast-schema-graph")
+    claim_review_graph_article = claim_review["@graph"].select{|x| x["@type"] == "Article"}[0]
+    claim_review_graph_author = claim_review["@graph"].select{|x| x["@type"] == ["Person"]}[0]||{}
     {
       id: raw_claim_review['url'],
-      created_at: timestamp,
-      claim_review_headline: raw_claim_review["page"].search("div.detector-article h1.h2").text.strip,
-      claim_review_body: raw_claim_review["page"].search("div.detector-article div.mainInternalArticle__content p").collect(&:text).collect(&:strip).join(" "),
-      claim_review_image_url: og_image_url_from_raw_claim_review(raw_claim_review),
-      claim_review_result: claim_review_result,
-      claim_review_result_score: claim_review_result_score,
+      created_at: Time.parse(claim_review_graph_article["datePublished"]),
+      author: claim_review_graph_author["name"],
+      author_link: claim_review_graph_author["url"],
+      claim_review_headline: claim_review_graph_article["headline"],
+      claim_review_body: raw_claim_review['page'].search('div.herald-entry-content p').text,
+      claim_review_reviewed: nil,
+      claim_review_result: nil,
+      claim_review_result_score: nil,
       claim_review_url: raw_claim_review['url'],
+      raw_claim_review: claim_review
     }
   end
+  # include PaginatedReviewClaims
+  # def hostname
+  #   'https://www.lasillavacia.com'
+  # end
+  #
+  # def fact_list_path(page = 1)
+  #   # appears to be zero-indexed
+  #   "/elementAjax/SillaDetector/MasHistoriasRecientes?page=#{page}"
+  # end
+  #
+  # def url_extraction_search
+  #   'a'
+  # end
+  #
+  # def url_extractor(atag)
+  #   atag.attributes['href'].value
+  # end
+  #
+  # def get_title_classes_from_raw_claim_review(raw_claim_review)
+  #   raw_claim_review["page"].search("div.detector-article h1.h2").first.attributes["class"].value
+  # end
+  #
+  # def claim_review_result_and_score_from_title_classes(title_classes)
+  #   if title_classes.include?("border-scale-red")
+  #     return [0.0, "False"]
+  #   elsif title_classes.include?("border-scale-orange")
+  #     return [0.33, "Mostly False"]
+  #   elsif title_classes.include?("border-scale-ligth-green")
+  #     return [0.66, "Mostly True"]
+  #   elsif title_classes.include?("border-scale-green")
+  #     return [1.0, "True"]
+  #   end
+  # end
+  #
+  # def parse_raw_claim_review(raw_claim_review)
+  #   timestamp = Time.parse(raw_claim_review["page"].search("div.detector-article time.p").first.text.strip) rescue nil
+  #   claim_review_result, claim_review_result_score = claim_review_result_and_score_from_title_classes(get_title_classes_from_raw_claim_review(raw_claim_review))
+  #   {
+  #     id: Digest::MD5.hexdigest(raw_claim_review['url']),
+  #     created_at: timestamp,
+  #     claim_review_headline: raw_claim_review["page"].search("div.detector-article h1.h2").text.strip,
+  #     claim_review_body: raw_claim_review["page"].search("div.detector-article div.mainInternalArticle__content p").collect(&:text).collect(&:strip).join(" "),
+  #     claim_review_image_url: og_image_url_from_raw_claim_review(raw_claim_review),
+  #     claim_review_result: claim_review_result,
+  #     claim_review_result_score: claim_review_result_score,
+  #     claim_review_url: raw_claim_review['url'],
+  #   }
+  # end
 end
