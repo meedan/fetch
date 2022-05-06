e1db51378e11b1d1e094f2f85c76b2fc90bade3f
Devin Gaffney <itsme@devingaffney.com>
2022-05-06 13:19:59 -0700

    CHECK-1824 cover different types of author inputs

--- lib/claim_review_parsers/hindi_boom_live.rb
+++ lib/claim_review_parsers/hindi_boom_live.rb
@@ -20,13 +20,26 @@ class HindiBoomLive < ClaimReviewParser
     hostname + atag.attributes['href'].value
   end
 
+  def get_article_author(article)
+    if article && article["author"] && article["author"].class == Array
+      {
+        author: article && article["author"][0] && article["author"][0]["name"],
+        author_link: article && article["author"][0] && hostname+article["author"][0]["url"],      
+      }
+    else
+      {
+        author: article && article["author"] && article["author"]["name"],
+        author_link: article && article["author"] && hostname+article["author"]["url"],      
+      }
+    end
+  end
+
   def parse_raw_claim_review(raw_claim_review)
     article = extract_ld_json_script_block(raw_claim_review["page"], 0)
+    article && article["author"] && article["author"]["name"] rescue binding.pry
     {
       id: raw_claim_review["url"],
       created_at: Time.parse(article['datePublished']||og_date_from_raw_claim_review(raw_claim_review)),
-      author: article && article["author"] && article["author"]["name"],
-      author_link: article && article["author"] && hostname+article["author"]["url"],
       claim_review_headline: article["claimReviewed"],
       claim_review_body: raw_claim_review["page"].search("div.single-post-summary h2").text,
       claim_review_reviewed: article["itemReviewed"] && article["itemReviewed"]["name"],
@@ -35,6 +48,8 @@ class HindiBoomLive < ClaimReviewParser
       claim_review_result_score: claim_result_score_from_raw_claim_review(article["reviewRating"]),
       claim_review_url: raw_claim_review['url'],
       raw_claim_review: article
-    }
+    }.merge(
+      get_article_author(article)
+    )
   end
 end
