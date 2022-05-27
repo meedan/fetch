2858aff41bff640cb1fdc416f5a00a225358054a
Devin Gaffney <itsme@devingaffney.com>
2022-05-27 08:11:47 -0700

    CHECK-1864 Add Desi Facts parser

---
+++ lib/claim_review_parsers/desi_facts.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# Parser for https://www.desifacts.org/
+class DesiFacts < ClaimReviewParser
+  include PaginatedReviewClaims
+  def hostname
+    'https://www.desifacts.org'
+  end
+
+  def is_claim_or_explainer(image_title_node)
+    image_title_node.text.downcase.include?("claim") || image_title_node.text.downcase.include?("explainer")
+  end
+
+  def is_article_url(url)
+    image_node = url.children.select{|x| x.name == "image"}.first
+    image_title_node = image_node && image_node.children.select{|x| x.name == "title"}.first
+    image_node && image_title_node && is_claim_or_explainer(image_title_node)
+  end
+
+  def get_article_urls
+    Nokogiri.parse(get_url(hostname+"/sitemap.xml")).search("url").select{|url| is_article_url(url)}.collect{|x| x.search("loc").text}
+  end
+  
+  def get_new_article_urls
+    page_urls = get_article_urls
+    page_urls-get_existing_urls(page_urls)
+  end
+
+  def get_claim_reviews
+    get_parsed_fact_pages_from_urls(get_new_article_urls)
+  end
+
+  def find_claim_review(page)
+    extract_all_ld_json_script_blocks(page).collect{|b| JSON.parse(b.text)}.select{|x| x["@type"] == "Article"}.first
+  end
+
+  def parse_raw_claim_review(raw_claim_review)
+    article = find_claim_review(raw_claim_review["page"]) || {}
+    timestamp = [article["datePublished"], article["dateModified"]].compact.sort.first
+    claim_review_result = raw_claim_review["page"].search("figcaption div.image-caption p").text
+    {
+      id: raw_claim_review['url'],
+      created_at: timestamp && Time.parse(timestamp),
+      author: article["author"],
+      claim_review_headline: article["headline"],
+      claim_review_body: raw_claim_review["page"].search("p.sqsrte-large em").text,
+      claim_review_image_url: article["image"],
+      claim_review_result: claim_review_result,
+      claim_review_result_score: claim_review_result == "True" ? 1 : 0,
+      claim_review_url: raw_claim_review['url'],
+      raw_claim_review: article
+    }
+  end
+end
---
+++ spec/lib/claim_review_parsers/desi_facts_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+describe DesiFacts do
+  before do
+    stub_request(:get, "https://www.desifacts.org/sitemap.xml").
+      with(
+        headers: {
+    	  'Accept'=>'*/*',
+    	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    	  'Host'=>'www.desifacts.org',
+        "User-Agent": /.*/
+        }).
+      to_return(status: 200, body: File.read("spec/fixtures/desi_facts_index_raw.xml"), headers: {})
+  end
+  describe 'instance' do
+    it 'has a hostname' do
+      expect(described_class.new.hostname).to(eq('https://www.desifacts.org'))
+    end
+
+    it 'runs get_claim_reviews' do
+      described_class.any_instance.stub(:get_existing_urls).with(anything).and_return(described_class.new.get_article_urls)
+      expect(described_class.new.get_claim_reviews).to(eq([]))
+    end
+
+    it 'parses a raw_claim_review' do
+      raw = JSON.parse(File.read('spec/fixtures/desi_facts_raw.json'))
+      raw['page'] = Nokogiri.parse(raw['page'])
+      parsed_claim = described_class.new.parse_raw_claim_review(raw)
+      expect(parsed_claim.class).to(eq(Hash))
+      ClaimReview.mandatory_fields.each do |field|
+        expect(QuietHashie[parsed_claim][field].nil?).to(eq(false))
+      end
+    end
+  end
+end
