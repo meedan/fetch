bf11a92f152d3af7c7ec6eb2c0b03a15bf847a0d
Devin Gaffney <itsme@devingaffney.com>
2022-07-25 12:48:17 -0700

    Meedan 2059 update vera files (#263)
    * CHECK-2059 update vera files parser
    
    * CHECK-2059 update function call

--- lib/claim_review_parsers/vera_files.rb
+++ lib/claim_review_parsers/vera_files.rb
@@ -24,14 +24,24 @@ class VeraFiles < ClaimReviewParser
     atag.attributes['href'].value.include?("/articles/") ? atag.attributes['href'].value : nil
   end
 
+  def get_claim_review_reviewed_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("span#claim_reviewed").text
+  end
+
+  def get_claim_review_result_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("span#review-rating").text
+  end
+
   def parse_raw_claim_review(raw_claim_review)
     {
       id: raw_claim_review['url'],
       created_at: Time.parse(og_date_from_raw_claim_review(raw_claim_review)),
       author: raw_claim_review["page"].search("div.entry-meta span#article_author")[-1].text,
       claim_review_headline: raw_claim_review["page"].search("article h1.article_title").text,
-      claim_review_body: raw_claim_review["page"].search("article div.entry-content p").collect(&:text).join(" "),
+      claim_review_body: raw_claim_review["page"].search("article div.entry-content p").first.text,
       claim_review_image_url: value_from_og_tag(search_for_og_tags(raw_claim_review["page"], ["og:image"])),
+      claim_review_reviewed: get_claim_review_reviewed_from_raw_claim_review(raw_claim_review),
+      claim_review_result: get_claim_review_result_from_raw_claim_review(raw_claim_review),
       claim_review_url: raw_claim_review['url']
     }
   end
