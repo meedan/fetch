17125267dcefe313f75524c146da634fdd62ec55
Devin Gaffney <itsme@devingaffney.com>
2022-08-18 08:56:36 -0700

    CHECK-2265 add two more afp sources

---
+++ lib/claim_review_parsers/afp_factual_all.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Parser for https://factual.afp.com, subclass of AFP's parser
+require_relative('afp')
+class AFPFactualAll < AFP
+  include PaginatedReviewClaims
+  def hostname
+    'https://factual.afp.com/list/all/all/all/38560/44'
+  end
+
+  def parse_raw_claim_review(raw_claim_review)
+    parsed = super(raw_claim_review)
+    parsed[:claim_review_body] = raw_claim_review["page"].search("article div.article-entry h3").text
+    parsed
+  end
+end
