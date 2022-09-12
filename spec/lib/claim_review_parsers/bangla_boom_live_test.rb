bf6bbdb50e1d499946ff162f5a6b963e32f03c54
Devin Gaffney <itsme@devingaffney.com>
2022-09-12 13:10:50 -0700

    CHECK-2397 fix minor parser error on some bangla articles with multiple authors (#286)
    * CHECK-2397 fix minor parser error on some bangla articles with multiple authors
    
    * CHECK-2397 accidentally a paren
    
    * CHECK-2397 accidentally a paren

--- spec/lib/claim_review_parsers/bangla_boom_live_test.rb
+++ spec/lib/claim_review_parsers/bangla_boom_live_test.rb
@@ -17,6 +17,18 @@ describe BanglaBoomLive do
     it 'extracts a url' do
       expect(described_class.new.url_extractor(Nokogiri.parse("<a href='/blah'>wow</a>").search('a')[0])).to(eq('http://bangla.boomlive.in/blah'))
     end
+    
+    it 'extracts author from array' do
+      article = {"author"=>
+        [{"@type"=>"Person", "name"=>"Sista Mukherjee", "url"=>"https://bangla.boomlive.in/author-sista", "jobTitle"=>"Editor", "image"=>"https://bangla.boomlive.in/h-upload/2022/07/14/981312-sista-new.webp", "sameAs"=>[]},
+         {"@type"=>"Person", "name"=>"Srijit Das", "url"=>"https://bangla.boomlive.in/srijit-das", "jobTitle"=>"Editor", "image"=>"https://bangla.boomlive.in/h-upload/2022/07/14/981312-sista-new.webp", "sameAs"=>["https://www.twitter.com/srijitofficial"]}]}
+      expect(described_class.new.get_author_and_link_from_article(article)).to(eq(["Sista Mukherjee", "http://bangla.boomlive.inhttps://bangla.boomlive.in/author-sista"]))
+    end
+
+    it 'gracefully nils on bad article author' do
+      article = {"author"=>"blah"}
+      expect(described_class.new.get_author_and_link_from_article(article)).to(eq([nil,nil]))
+    end
 
     it 'parses a raw_claim_review' do
       raw = JSON.parse(File.read('spec/fixtures/bangla_boom_live_raw.json'))
