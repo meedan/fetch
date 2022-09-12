a34873378845dcaa44278d348c1758a17091d2f0
Devin Gaffney <itsme@devingaffney.com>
2022-09-12 11:39:58 -0700

    CHECK-2397 fix minor parser error on some bangla articles with multiple authors

--- lib/claim_review_parsers/bangla_boom_live.rb
+++ lib/claim_review_parsers/bangla_boom_live.rb
@@ -20,13 +20,26 @@ class BanglaBoomLive < ClaimReviewParser
     hostname + atag.attributes['href'].value
   end
 
+  def get_author_and_link_from_article(article)
+    if article && article["author"]
+      if article["author"].class == Hash
+        return [article["author"]["name"], hostname+article["author"]["url"].to_s]
+      elsif article["author"].class == Array
+        return [article["author"][0]["name"], hostname+article["author"][0]["url"].to_s]
+      else
+        return [nil,nil]
+      end
+    end
+  end
+
   def parse_raw_claim_review(raw_claim_review)
     article = extract_ld_json_script_block(raw_claim_review["page"], 0)
+    author, author_link = get_author_and_link_from_article(article)
     {
       id: raw_claim_review["url"],
       created_at: Time.parse(article['datePublished']||og_date_from_raw_claim_review(raw_claim_review)),
-      author: article && article["author"] && article["author"]["name"],
-      author_link: article && article["author"] && hostname+article["author"]["url"],
+      author: author,
+      author_link: author_link,
       claim_review_headline: article["claimReviewed"],
       claim_review_body: raw_claim_review["page"].search("div.single-post-summary h2").text,
       claim_review_reviewed: article["itemReviewed"] && article["itemReviewed"]["name"],
