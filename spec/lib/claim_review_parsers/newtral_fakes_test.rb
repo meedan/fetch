5f1478e772ac538e183dabdf5136c3f6a845c32f
Devin Gaffney <itsme@devingaffney.com>
2022-10-27 12:06:20 -0700

    CHECK-2638 overhaul newtral indexing strategy

--- spec/lib/claim_review_parsers/newtral_fakes_test.rb
+++ spec/lib/claim_review_parsers/newtral_fakes_test.rb
@@ -6,8 +6,43 @@ describe NewtralFakes do
       expect(described_class.new.hostname).to(eq('https://www.newtral.es'))
     end
 
-    it 'has a fact_list_path' do
-      expect(described_class.new.fact_list_path(1)).to(eq("/wp-json/nwtfmg/v1/fakes?page=1&posts_per_page=15&firstDate=2018-01-01&lastDate=#{Time.now.strftime("%Y-%m-%d")}"))
+    it 'has a relevant_sitemap_subpath' do
+      expect(described_class.new.relevant_sitemap_subpath).to(eq("www.newtral.es/fake"))
+    end
+
+    it 'matches urls based on subpath' do
+      expect(described_class.new.includes_relevant_path("https://www.newtral.es/fake-sitemap.xml")).to(eq(true))
+    end
+
+    it 'gets sitemap urls' do
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/sitemap_index.xml").and_return(File.read("spec/fixtures/newtral_sitemap_main.xml"))
+      response = described_class.new.get_sitemap_urls("https://www.newtral.es/sitemap_index.xml")
+      expect(response.class).to(eq(Array))
+      expect(response[0].class).to(eq(String))
+    end
+
+    it 'gets article urls' do
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/sitemap_index.xml").and_return(File.read("spec/fixtures/newtral_sitemap_main.xml"))
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/fake-sitemap.xml").and_return(File.read("spec/fixtures/newtral_sitemap_sub_tree.xml"))
+      response = described_class.new.get_article_urls
+      expect(response.class).to(eq(Array))
+      expect(response[0].class).to(eq(String))
+    end
+
+    it 'gets new article urls' do
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/sitemap_index.xml").and_return(File.read("spec/fixtures/newtral_sitemap_main.xml"))
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/fake-sitemap.xml").and_return(File.read("spec/fixtures/newtral_sitemap_sub_tree.xml"))
+      described_class.any_instance.stub(:get_existing_urls).with(anything).and_return([])
+      response = described_class.new.get_new_article_urls
+      expect(response.class).to(eq(Array))
+      expect(response[0].class).to(eq(String))
+    end
+
+    it 'runs get_claim_reviews' do
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/sitemap_index.xml").and_return(File.read("spec/fixtures/newtral_sitemap_main.xml"))
+      described_class.any_instance.stub(:get_url).with("https://www.newtral.es/fake-sitemap.xml").and_return(File.read("spec/fixtures/newtral_sitemap_sub_tree.xml"))
+      described_class.any_instance.stub(:get_existing_urls).with(anything).and_return(described_class.new.get_article_urls)
+      expect(described_class.new.get_claim_reviews).to(eq([]))
     end
 
     it 'parses a raw_claim_review' do
