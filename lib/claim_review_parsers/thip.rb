e92900061d67c9514829433f888a0f6f1c0f98c6
Devin Gaffney <itsme@devingaffney.com>
2022-12-13 07:08:50 -0800

    CHECK-2675 update parser to use JSON data only

--- lib/claim_review_parsers/thip.rb
+++ lib/claim_review_parsers/thip.rb
@@ -2,10 +2,22 @@
 
 # Parser for https://www.thip.media
 class Thip < ClaimReviewParser
+  attr_accessor :raw_response
   include PaginatedReviewClaims
   def initialize(cursor_back_to_date = nil, overwrite_existing_claims=false, send_notifications = true)
     super(cursor_back_to_date, overwrite_existing_claims, send_notifications)
     @fact_list_page_parser = 'json'
+    @raw_response = {}
+  end
+
+  def get_new_fact_page_urls(page)
+    response = get_fact_page_urls(page)
+    existing_urls = get_existing_urls(response.collect{|d| d["link"]})
+    response.select{|d| !existing_urls.include?(d["link"])}
+  end
+
+  def parsed_fact_page(fact_page_response)
+    [fact_page_response["link"], parse_raw_claim_review(QuietHashie[{ raw_response: fact_page_response, url: fact_page_response["link"] }])]
   end
 
   def hostname
@@ -17,27 +29,22 @@ class Thip < ClaimReviewParser
   end
 
   def url_extractor(response)
-    response.collect{|x| x["link"]}
-  end
-
-  def get_claim_review_safely(raw_claim_review)
-    claim_review = extract_ld_json_script_block(raw_claim_review["page"], 0)
-    claim_review && claim_review["@graph"] && claim_review["@graph"][0] || {}
+    response
   end
 
   def parse_raw_claim_review(raw_claim_review)
-    claim_review = get_claim_review_safely(raw_claim_review)
+    parsed_page = Nokogiri.parse("<html>"+raw_claim_review["raw_response"]["content"]["rendered"]+"</html>")
     {
       id: raw_claim_review['url'],
-      created_at: (Time.parse(claim_review["datePublished"]) rescue nil),
-      claim_review_headline: claim_review["name"].split(" &ndash;")[0..-2].join(" &ndash;"),
-      claim_review_body: raw_claim_review["page"].search("div.wp-block-media-text").first.text.strip,
-      claim_review_image_url: get_og_image_url(raw_claim_review),
-      claim_review_reviewed: claim_review["claimReviewed"],
-      claim_review_result: claim_review["reviewRating"] && claim_review["reviewRating"]["alternateName"],
-      claim_review_result_score: claim_result_score_from_raw_claim_review(claim_review),
+      author: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["display_name"],
+      author_link: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["author_link"],
+      created_at: (Time.parse(raw_claim_review["raw_response"]["date"]) rescue nil),
+      claim_review_headline: raw_claim_review["raw_response"]["yoast_head_json"]["title"].split(" - ")[0..-2].join(" - "),
+      claim_review_body: parsed_page.search("div.wp-block-media-text").first.text.strip,
+      claim_review_image_url: raw_claim_review["raw_response"]["featured_img"],
+      claim_review_result: parsed_page.search("p.has-regular-font-size strong").text.gsub(".", ""),
       claim_review_url: raw_claim_review['url'],
-      raw_claim_review: claim_review
+      raw_claim_review: raw_claim_review["raw_response"]
     }
   end
 end
--- spec/lib/claim_review_parsers/thip_test.rb
+++ spec/lib/claim_review_parsers/thip_test.rb
@@ -10,18 +10,39 @@ describe Thip do
      	  'Host'=>'www.thip.media',
     	  'User-Agent'=>/.*/
         }).
-        to_return(status: 200, body: File.read("spec/fixtures/thip_index_raw.json"), headers: {})
+        to_return(status: 200, body: JSON.parse(File.read("spec/fixtures/thip_index_raw.json")), headers: {})
   end
 
   describe 'instance' do
-    it 'runs get_fact_page_urls across multiple hostnames' do
-      response = described_class.new.get_fact_page_urls(1)
-      expect(response.class).to(eq(Array))
+    it 'has a hostname' do
+      expect(described_class.new.hostname).to(eq('https://www.thip.media'))
     end
+
+    it 'has a fact_list_path' do
+      expect(described_class.new.fact_list_path(1)).to(eq('/wp-json/wp/v2/posts?categories=27,28,162,164,166,168,1886,1994,520&per_page=100&page=1'))
+    end
+
+    it 'extracts a url' do
+      expect(described_class.new.url_extractor([{"url" => "/blah"}])).to(eq([{"url" => "/blah"}]))
+    end
+
+    it 'extracts raw responses from get_new_fact_page_urls' do
+      described_class.any_instance.stub(:get_fact_page_urls).with(1).and_return([{"content" => {"url" => "/blah"}}])
+      described_class.any_instance.stub(:get_existing_urls).with(anything()).and_return([])
+      expect(described_class.new.get_new_fact_page_urls(1)).to(eq([{"content"=>{"url"=>"/blah"}}]))
+    end
+
+    it 'extracts parsed_fact_page results' do
+      keys = [:author, :author_link, :claim_review_body, :claim_review_headline, :claim_review_image_url, :claim_review_result, :claim_review_url, :created_at, :id, :raw_claim_review].sort
+      response = described_class.new.parsed_fact_page(JSON.parse(File.read('spec/fixtures/thip_raw.json'))["raw_response"])
+      expect(response[0]).to(eq('https://www.thip.media/health-news-fact-check/fact-check-can-vitamin-d-cure-cancer/36698/'))
+      expect(response[1].class).to(eq(Hash))
+      expect(response[1].keys.sort).to(eq(keys))
+    end
+    
     it 'parses a raw_claim_review' do
       raw = JSON.parse(File.read('spec/fixtures/thip_raw.json'))
-      raw['page'] = Nokogiri.parse(raw['page'])
-      parsed_claim = described_class.new.parse_raw_claim_review(raw)
+      parsed_claim = Thip.new.parse_raw_claim_review(raw)
       expect(parsed_claim.class).to(eq(Hash))
       ClaimReview.mandatory_fields.each do |field|
         expect(QuietHashie[parsed_claim][field].nil?).to(eq(false))
