d385fa9e8af746dda5dcb0d2cd0b86e66d39cc02
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-02-21 17:18:01 -0300

    updated the stub_request for pender_client_test
    Same issue as the alegre_client, we were getting an error
    because of the Host. Since it's not necessary we removed it.
    Also updated to use Settings.get_safe_url instead of hardcoding
    the url.

--- spec/lib/pender_client_test.rb
+++ spec/lib/pender_client_test.rb
@@ -13,21 +13,19 @@
 #
 describe PenderClient do
   before do
-    stub_request(:get, "http://pender.local/api/medias.json?url=https://twitter.com/meedan/status/773947372527288320/").
+    stub_request(:get, "#{Settings.get_safe_url("pender_host_url")}api/medias.json?url=https://twitter.com/meedan/status/773947372527288320/").
       with(
         headers: {
-      	  'Accept'=>'*/*',
-      	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      	  'Host'=>'pender.local',
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
           "User-Agent": /.*/
         }).
       to_return(status: 200, body: File.read("spec/fixtures/pender_response.json"), headers: {})
-    stub_request(:get, "http://pender.local/api/medias.json?url=http://example.com/link").
+    stub_request(:get, "#{Settings.get_safe_url("pender_host_url")}api/medias.json?url=http://example.com/link").
       with(
         headers: {
-      	  'Accept'=>'*/*',
-      	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      	  'Host'=>'pender.local',
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
           "User-Agent": /.*/
         }).
       to_return(status: 200, body: File.read("spec/fixtures/pender_response.json"), headers: {})
@@ -37,11 +35,11 @@ describe PenderClient do
     it 'expects no pender response' do
       expect(PenderClient.get_enrichment_for_url("http://example.com/link")).to(eq({}))
     end
-    
+
     it 'expects a pender response' do
       expect(PenderClient.get_enrichment_for_url("https://twitter.com/meedan/status/773947372527288320/")).to(eq(JSON.parse(File.read("spec/fixtures/pender_response.json"))))
     end
-    
+
     it 'degrades gracefully when Alegre errors out' do
       RestClient::ServiceUnavailable.any_instance.stub(:http_code).and_return(500)
       RestClient::Request.stub(:execute).and_raise(RestClient::ServiceUnavailable.new)
