f4b6a9b2c5cdf9dfabca63f4d69aded04097f9d4
Devin Gaffney <itsme@devingaffney.com>
2023-02-27 16:54:09 -0800

    CV2-2778 add test coverage for eventually-correct answer

--- spec/lib/claim_review_parsers/estadao_test.rb
+++ spec/lib/claim_review_parsers/estadao_test.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 describe Estadao do
+  before do
+    stub_request(:get, "https://www.estadao.com.br/").
+      with(
+        headers: {
+    	  'Accept'=>'*/*',
+    	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    	  'Host'=>'www.estadao.com.br',
+        }).
+      to_return(status: 200, body: '<html><link rel="shortcut icon" href="/pf/resources/favicon.ico?d=556"/></html>', headers: {})
+  end
+
   describe 'instance' do
     it 'has a hostname' do
       expect(described_class.new.hostname).to(eq('https://www.estadao.com.br'))
     end
 
     it 'has a fact_list_path' do
-      expect(described_class.new.fact_list_path(1)).to(eq('/pf/api/v3/content/fetch/story-feed-query?query=%7B%22body%22:%22%7B%5C%22query%5C%22:%7B%5C%22bool%5C%22:%7B%5C%22must%5C%22:[%7B%5C%22term%5C%22:%7B%5C%22type%5C%22:%5C%22story%5C%22%7D%7D,%7B%5C%22term%5C%22:%7B%5C%22revision.published%5C%22:1%7D%7D,%7B%5C%22nested%5C%22:%7B%5C%22path%5C%22:%5C%22taxonomy.sections%5C%22,%5C%22query%5C%22:%7B%5C%22bool%5C%22:%7B%5C%22must%5C%22:[%7B%5C%22regexp%5C%22:%7B%5C%22taxonomy.sections._id%5C%22:%5C%22.*estadao-verifica.*%5C%22%7D%7D]%7D%7D%7D%7D]%7D%7D%7D%22,%22size%22:0,%22sort%22:%22display_date:desc,%20first_publish_date:desc%22%7D&d=539&_website=estadao'))
+      expect(described_class.new.fact_list_path(1)).to(eq('/pf/api/v3/content/fetch/story-feed-query?query=%7B%22body%22:%22%7B%5C%22query%5C%22:%7B%5C%22bool%5C%22:%7B%5C%22must%5C%22:[%7B%5C%22term%5C%22:%7B%5C%22type%5C%22:%5C%22story%5C%22%7D%7D,%7B%5C%22term%5C%22:%7B%5C%22revision.published%5C%22:1%7D%7D,%7B%5C%22nested%5C%22:%7B%5C%22path%5C%22:%5C%22taxonomy.sections%5C%22,%5C%22query%5C%22:%7B%5C%22bool%5C%22:%7B%5C%22must%5C%22:[%7B%5C%22regexp%5C%22:%7B%5C%22taxonomy.sections._id%5C%22:%5C%22.*estadao-verifica.*%5C%22%7D%7D]%7D%7D%7D%7D]%7D%7D%7D%22,%22size%22:0,%22sort%22:%22display_date:desc,%20first_publish_date:desc%22%7D&d=556&_website=estadao'))
     end
 
     it 'extracts a url' do
@@ -20,6 +31,7 @@ describe Estadao do
       raw['page'] = Nokogiri.parse(raw['page'])
       parsed_claim = described_class.new.parse_raw_claim_review(raw)
       expect(parsed_claim[:claim_review_image_url].class).to(eq(String))
+      described_class.any_instance.unstub(:extract_ld_json_script_block)
     end
 
     it 'parses a raw_claim_review' do
