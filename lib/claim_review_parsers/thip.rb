8aea8ced5b9358113c16c044bdb4d89fa3a16d7e
manu vasconcelos <manu@meedan.com>
2023-05-08 19:43:22 -0300

    wip - palying around with the ld_json response

--- lib/claim_review_parsers/thip.rb
+++ lib/claim_review_parsers/thip.rb
@@ -33,16 +33,30 @@ class Thip < ClaimReviewParser
   end
 
   def parse_raw_claim_review(raw_claim_review)
-    parsed_page = Nokogiri.parse("<html>"+raw_claim_review["raw_response"]["content"]["rendered"]+"</html>")
+    claim_review = extract_ld_json_script_block(raw_claim_review["page"], 0)
+    binding.pry
+    # parsed_page = Nokogiri.parse("<html>"+raw_claim_review["raw_response"]["content"]["rendered"]+"</html>")
+    # {
+    #   id: raw_claim_review['url'],
+    #   author: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["display_name"],
+    #   author_link: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["author_link"],
+    #   created_at: (Time.parse(raw_claim_review["raw_response"]["date"]) rescue nil),
+    #   claim_review_headline: raw_claim_review["raw_response"]["yoast_head_json"]["title"].split(" - ")[0..-2].join(" - "),
+    #   claim_review_body: (parsed_page.search("div.wp-block-media-text").first.text.strip rescue parsed_page.text),
+    #   claim_review_image_url: raw_claim_review["raw_response"]["featured_img"],
+    #   claim_review_result: parsed_page.search("p.has-regular-font-size strong").text.gsub(".", ""),
+    #   claim_review_url: raw_claim_review['url'],
+    #   raw_claim_review: raw_claim_review["raw_response"]
+    # }
     {
       id: raw_claim_review['url'],
-      author: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["display_name"],
-      author_link: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["author_link"],
-      created_at: (Time.parse(raw_claim_review["raw_response"]["date"]) rescue nil),
-      claim_review_headline: raw_claim_review["raw_response"]["yoast_head_json"]["title"].split(" - ")[0..-2].join(" - "),
-      claim_review_body: (parsed_page.search("div.wp-block-media-text").first.text.strip rescue parsed_page.text),
-      claim_review_image_url: raw_claim_review["raw_response"]["featured_img"],
-      claim_review_result: parsed_page.search("p.has-regular-font-size strong").text.gsub(".", ""),
+      author: claim_review["@graph"][8]["name"],
+      # author_link: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["author_link"],
+      created_at: (Time.parse(claim_review["@graph"][0]["dateCreated"]) rescue nil),
+      claim_review_headline: claim_review["@graph"][0]["name"].split(" ")[0..-2].join("-"),
+      claim_review_body: claim_review["@graph"][0]["description"],
+      # claim_review_image_url: raw_claim_review["raw_response"]["featured_img"],
+      claim_review_result: claim_review["@graph"][0]["reviewRating"]["alternateName"],
       claim_review_url: raw_claim_review['url'],
       raw_claim_review: raw_claim_review["raw_response"]
     }
