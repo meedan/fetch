beac3b45a72b78af8beb78fe034c522ba3abb395
Devin Gaffney <itsme@devingaffney.com>
2023-05-18 15:24:30 -0700

    Cv2 2849 titles not captured for some thip articles (#366)
    * wip - palying around with the ld_json response
    
    * update the way we remove ' - THIP Media'
    
    The way we were doing this before was causing a few empty strings for
    the headlines. We also added raw_claim_review["raw_response"]["title"]["rendered"]
    as a fallback, in case some of the titles are truly missing.
    
    I looked into using the ld_json, but this meant parsing the url twice,
    so we didn't go with it.
    
    ---------
    
    Co-authored-by: manu vasconcelos <manu@meedan.com>

--- lib/claim_review_parsers/thip.rb
+++ lib/claim_review_parsers/thip.rb
@@ -4,7 +4,7 @@
 class Thip < ClaimReviewParser
   attr_accessor :raw_response
   include PaginatedReviewClaims
-  def initialize(cursor_back_to_date = nil, overwrite_existing_claims=false, send_notifications = true)
+  def initialize(cursor_back_to_date = nil, overwrite_existing_claims = false, send_notifications = true)
     super(cursor_back_to_date, overwrite_existing_claims, send_notifications)
     @fact_list_page_parser = 'json'
     @raw_response = {}
@@ -32,19 +32,28 @@ class Thip < ClaimReviewParser
     response
   end
 
+  def get_headline(raw_claim_review)
+    if raw_claim_review["raw_response"]["yoast_head_json"]["title"].empty?
+        raw_claim_review["raw_response"]["title"]["rendered"].include?("THIP Media") ? raw_claim_review["raw_response"]["title"]["rendered"].split(/ &ndash; | - /)[0..-2].join(" ") : raw_claim_review["raw_response"]["yoast_head_json"]["title"]
+    else
+      raw_claim_review["raw_response"]["yoast_head_json"]["title"].include?("THIP Media") ? raw_claim_review["raw_response"]["yoast_head_json"]["title"].split(/ &ndash; | - /)[0..-2].join(" ") : raw_claim_review["raw_response"]["yoast_head_json"]["title"]
+    end
+  end
+
   def parse_raw_claim_review(raw_claim_review)
     parsed_page = Nokogiri.parse("<html>"+raw_claim_review["raw_response"]["content"]["rendered"]+"</html>")
+
     {
       id: raw_claim_review['url'],
       author: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["display_name"],
       author_link: raw_claim_review["raw_response"]["author_meta"] && raw_claim_review["raw_response"]["author_meta"]["author_link"],
       created_at: (Time.parse(raw_claim_review["raw_response"]["date"]) rescue nil),
-      claim_review_headline: raw_claim_review["raw_response"]["yoast_head_json"]["title"].split(" - ")[0..-2].join(" - "),
+      claim_review_headline: get_headline(raw_claim_review),
       claim_review_body: (parsed_page.search("div.wp-block-media-text").first.text.strip rescue parsed_page.text),
       claim_review_image_url: raw_claim_review["raw_response"]["featured_img"],
       claim_review_result: parsed_page.search("p.has-regular-font-size strong").text.gsub(".", ""),
       claim_review_url: raw_claim_review['url'],
-      raw_claim_review: raw_claim_review["raw_response"]
+      raw_claim_review: raw_claim_review["raw_response"],
     }
   end
 end
