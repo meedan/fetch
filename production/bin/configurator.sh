15549a9b5c3f018963a21eda0e06e9137a77e6dc
Devin Gaffney <itsme@devingaffney.com>
2023-10-16 13:25:26 -0700

    Update configurator.sh

--- production/bin/configurator.sh
+++ production/bin/configurator.sh
@@ -2,17 +2,5 @@
 
 set -e
 
-if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
-	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
-	exit 1
-fi
-
 cp config/cookies.json.example config/cookies.json
-
-if [[ "$DEPLOY_ENV" != "qa" && "$DEPLOY_ENV" != "live" ]]; then
-	# Only use the test configuration if we're not deploying to QA or Live.
-	cp .env_file.test .env_file
-else
-	# The dumb-init process expects an .env_file. Use an empty one for QA and Live.
-	touch .env_file
-fi
+touch .env_file
