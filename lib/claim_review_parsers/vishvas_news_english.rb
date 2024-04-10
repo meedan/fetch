a6be481b4e73be575a296a45a2713c1067c3ba8c
Manu Vasconcelos <87862340+vasconsaurus@users.noreply.github.com>
2024-04-10 08:33:26 -0300

    deprecate vishvas parsers (#389)
    Vishvas News fact-checks are now being imported through Zapier.

--- lib/claim_review_parsers/vishvas_news_english.rb
+++ lib/claim_review_parsers/vishvas_news_english.rb
@@ -3,6 +3,10 @@
 # Parser for https://www.vishvasnews.com
 class VishvasNewsEnglish < VishvasNews
   include PaginatedReviewClaims
+  def self.deprecated
+    true
+  end
+
   def fact_page_params(page)
     {
       action: "ajax_pagination",
