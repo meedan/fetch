f10f05a778d20466afaae0d511bccb9a1638e384
manu vasconcelos <manu@meedan.com>
2024-04-15 08:57:30 -0300

    deprecate newschecker
    It is now importing fact-checks through the zapier/check integration.

--- lib/claim_review_parsers/news_checker.rb
+++ lib/claim_review_parsers/news_checker.rb
@@ -3,6 +3,10 @@
 # Parser for https://newschecker.in
 class NewsChecker < ClaimReviewParser
   include PaginatedReviewClaims
+  def self.deprecated
+    true
+  end
+
   def hostnames
     [
       "https://newschecker.in",
